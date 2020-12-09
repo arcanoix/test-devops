acd6ddfc6c347dc808fa9531a343348b0323776e
Gustavo Herrera <gustavoh.2312@gmail.com>
2020-12-09 12:20:35 -0500

    initial project

--- server.js
+++ server.js
@@ -3,6 +3,27 @@ const _server = _express();
 
 const _port = 4000;
 
+const mysql = require('mysql');
+
+
+
+const db = mysql.createConnection ({
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'ibm_test_db'
+});
+
+// connect to database
+db.connect((err) => {
+  if (err) {
+      throw err;
+  }
+  console.log('Connected to database');
+});
+global.db = db;
+
+
 _server.get('/retoibm/sumar/:sumando01/:sumando02', function(request, response) {
   try{
     var _sumando01 = new Number(request.params.sumando01);
@@ -10,7 +31,18 @@ _server.get('/retoibm/sumar/:sumando01/:sumando02', function(request, response) 
     var _resultado = _sumando01 + _sumando02;
     
     if (typeof _resultado !== "undefined" && _resultado!==null && !isNaN(_resultado)){    
-      return response.status(200).json({resultado : _resultado});
+        
+      let query = "INSERT INTO `sumatoria` (sumando01, sumando02, resultado) VALUES ('" + _sumando01 + "', '" + _sumando02 + "', '" + _resultado + "')";
+
+        db.query(query, (err, result) => {
+            if (err) {
+                return response.status(500).send(err);
+            }
+
+            return response.status(200).json({resultado : _resultado});
+
+        });
+
     }else{
       return response.status(400).json({resultado : "Bad Request"});
     }
